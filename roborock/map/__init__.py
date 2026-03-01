979ebf09c99ccb72463a6aa9a3162a28ba8128e8
arduano <leonid.shchurov@gmail.com>
2026-03-01 18:08:18 +1100

    🦎 q7: split 2/3 add B01 SCMap parser + renderer + testdata

--- roborock/map/__init__.py
+++ roborock/map/__init__.py
@@ -1,7 +1,12 @@
-"""Module for Roborock map related data classes."""
+"""Utilities and data classes for working with Roborock maps."""
 
+from .b01_map_parser import B01MapData, parse_scmap_payload, render_map_png
 from .map_parser import MapParserConfig, ParsedMapData
 
 __all__ = [
     "MapParserConfig",
+    "ParsedMapData",
+    "B01MapData",
+    "parse_scmap_payload",
+    "render_map_png",
 ]
